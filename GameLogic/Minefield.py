77284e3190f8a760b2cb9093afa4f1f7b5d62ea9
matthew-p-gallagher <mg95@hotmail.co.uk>
2022-06-16 23:30:10 +0100

    Add count neighbour mines function

--- GameLogic/Minefield.py
+++ GameLogic/Minefield.py
@@ -8,15 +8,17 @@ class Minefield:
     EMPTY = 0
 
     def __init__(self, field_size, no_of_bombs):
-        self.area = Minefield.area_init(field_size, no_of_bombs)
+        self.minefield = Minefield.create_minefield(field_size, no_of_bombs)
+        self.counted = Minefield.count_neighbours(self.minefield)
 
 
-    def area_init(n, b):
+    def create_minefield(n, b):
         # create 1D array and add bombs
         area = np.zeros(n*n, dtype=int)
         area[:b] = -1
 
         #randomise the bomb placement
+        np.random.seed(1000)
         np.random.shuffle(area)
 
         # shape into square grid
@@ -24,12 +26,44 @@ class Minefield:
         
         return area
 
-    def count_neighbours():
-        pass
+    def count_neighbours(minefield):
+
+        counted = np.copy(minefield)
+
+        x, y = minefield.shape
+        for i in range(x):
+            for j in range(y):
+
+                i1 = i - 1
+                i2 = i + 2
+                j1 = j - 1
+                j2 = j + 2
+
+                if i == 0:
+                    i1 = 0
+                elif i == x - 1:
+                    i2 = x
+
+                if j == 0:
+                    j1 = 0
+                elif j == y - 1:
+                    j2 = y
+
+                if minefield[i,j] == -1:
+                    continue
+                else:
+                    counted[i,j] = -1 * minefield[i1:i2, j1:j2].sum()
+                    
+        return counted
+
+                
+
 
 
     def display(self):
-        print(self.area)
+        print(self.minefield)
+        print()
+        print(self.counted)
 
 if __name__ == "__main__":
 
