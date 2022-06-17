13088a37b9e77340f7d3dad59f7685a7462237d5
matthew-p-gallagher <mg95@hotmail.co.uk>
2022-06-17 23:57:44 +0100

    commenting and adding to readme

--- GameLogic/Minefield.py
+++ GameLogic/Minefield.py
@@ -1,44 +1,50 @@
-
 import numpy as np
 
+
 class Minefield:
-    """A minesweeper board - nxn grid of either bomb or no. of neighbouring bombs"""
+    """A minesweeper board - n x n grid of either mine or no. of neighbouring mines"""
 
-    BOMB = -1
-    EMPTY = 0
+    def __init__(self, field_size, no_of_mines):
 
-    def __init__(self, field_size, no_of_bombs):
-        self.minefield = Minefield.create_minefield(field_size, no_of_bombs)
-        self.counted = Minefield.count_neighbours(self.minefield)
+        # create a minefield - an array of -1 for mine and 0 for empty
+        self.minefield = Minefield._create_minefield(field_size, no_of_mines)
 
+        # for empty squares count neighbouring mines
+        self.counted = Minefield._count_neighbours(self.minefield)
 
-    def create_minefield(n, b):
-        # create 1D array and add bombs
-        area = np.zeros(n*n, dtype=int)
+    def _create_minefield(n, b):
+
+        # create 1D array and add mines
+        area = np.zeros(n * n, dtype=int)
         area[:b] = -1
 
-        #randomise the bomb placement
+        # randomise the mine placement
+        # TODO remove seed
         np.random.seed(1000)
         np.random.shuffle(area)
 
         # shape into square grid
-        area = area.reshape((n,n))
-        
+        area = area.reshape((n, n))
+
         return area
 
-    def count_neighbours(minefield):
+    def _count_neighbours(minefield):
 
+        # create a new array rather than calculate in place
         counted = np.copy(minefield)
 
+        # iterate through all squares in the minefield
         x, y = minefield.shape
         for i in range(x):
             for j in range(y):
 
+                # coords for a 3 x 3 slice around square
                 i1 = i - 1
                 i2 = i + 2
                 j1 = j - 1
                 j2 = j + 2
-
+                
+                # change the slice of square is at the edge
                 if i == 0:
                     i1 = 0
                 elif i == x - 1:
@@ -49,21 +55,20 @@ class Minefield:
                 elif j == y - 1:
                     j2 = y
 
-                if minefield[i,j] == -1:
+                # if sqaure is a mine then move on
+                # otherwise get sum of neighbouring mines
+                if minefield[i, j] == -1:
                     continue
                 else:
-                    counted[i,j] = -1 * minefield[i1:i2, j1:j2].sum()
-                    
+                    counted[i, j] = -1 * minefield[i1:i2, j1:j2].sum()
+
         return counted
-
-                
-
-
 
     def display(self):
         print(self.minefield)
         print()
         print(self.counted)
+
 
 if __name__ == "__main__":
 
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # minecraftsweeper
 A Minesweeper game in a Python Minecraft engine
+
+## The minecraft game engine
+
+This project was built into an existing open source minecraft game. (link)) The minecraft engine is written entirely in Python using the pyglet package. It consists of 900 lines of Python code in a single file - an exemplar project for concise and effective code.
+
+## What I have added
+
+All the game logic for minesweeper was written from scratch. Other additions include the game menu at start up and the explosion effects on mine activation.
+
+## How to run
+
+After cloning the repository - navigate to the root folder in a command prompt terminal, run $pip install requirments (.txt), run $python minecraftsweeper
